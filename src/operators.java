53e44cc9e56fc09b37d0c46ca96d8e79ba3a3703
Thomas Egger <tegger01@yahoo.com>
2018-08-30 10:27:04 -0500

    Initial commit

---
+++ src/operators.java
@@ -0,0 +1,23 @@
+public class operators {
+
+
+
+
+
+        public static void main(String[] args) {
+            double a = 12;
+            double b = 15;
+            double c = a + b;
+            double d = c * a;
+            double e = d / b;
+            double f = e % a;
+
+            System.out.println(" 12 + 15 = " + c);
+            System.out.println(" 27 * 12 = " + d);
+            System.out.println(" 324 / 15 = " + e);
+            System.out.println(" 21.6 % 15 = " + f);
+
+        }
+    }
+
+
